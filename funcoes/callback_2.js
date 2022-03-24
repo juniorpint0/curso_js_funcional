f59b150a5421ccb3c1a88842ca1a29bfd21f1ca8
Pinto Júnior <junior_pinto@hotmail.com>
2022-03-24 20:34:29 -0300

    ok

--- funcoes/callback_2.js
+++ funcoes/callback_2.js
@@ -1,20 +1,20 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require('fs');
+const path = require('path');
 
-const caminho = path.join(__dirname, "dados.txt")
+const caminho = path.join(__dirname, 'dados.txt');
 
 function exibirConteudo(err, conteudo) {
-    console.log(conteudo.toString())
+	console.log(conteudo.toString());
 }
 
-console.log('Inicio...')
-fs.readFile(caminho, {}, exibirConteudo)
-fs.readFile(caminho, (_, conteudo)=> console.log(conteudo.toString())) // Quando não se quer passar o primeiro parâmetro, pode se usar underline _, o segundo parâmetro nesse caso pode ser suprimido.
-console.log('Fim...')
+console.log('Inicio...');
+fs.readFile(caminho, {}, exibirConteudo);
+fs.readFile(caminho, (_, conteudo) => console.log(conteudo.toString())); // Quando não se quer passar o primeiro parâmetro, pode se usar underline _, o segundo parâmetro nesse caso pode ser suprimido.
+console.log('Fim...');
 
 // fs.readFileSync(caminho)
 
-console.log('Inicio Sync...')
-const conteudo = fs.readFileSync(caminho)
-console.log(conteudo.toString())
-console.log('Fim Sync...')
+console.log('Inicio Sync...');
+const conteudo = fs.readFileSync(caminho);
+console.log(conteudo.toString());
+console.log('Fim Sync...');
