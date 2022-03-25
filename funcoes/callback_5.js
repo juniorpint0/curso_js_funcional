631076951715dbe89652647848aa54d1659734fd
Pinto Júnior <junior_pinto@hotmail.com>
2022-03-25 20:04:37 -0300

    ok

--- funcoes/callback_5.js
+++ funcoes/callback_5.js
@@ -5,11 +5,25 @@ const carrinho = [
 	{ nome: 'Lapis', qtde: 3, preco: 5.82 },
 	{ nome: 'Tesoura', qtde: 1, preco: 19.2 }
 ];
+
 const getTotal = (item) => item.qtde * item.preco;
-const somar = (acc, el) => {
-	return acc + el;
-};
+const somar = (acc, el) => acc + el;
 
 const totalGeral = carrinho.map(getTotal).reduce(somar);
 
 console.log(totalGeral);
+
+Array.prototype.meuReduce = function(fn, inicial) {
+	let acc = inicial;
+	for (let i = 0; i < this.length; i++) {
+		if (!acc && i === 0) {
+			acc = this[i];
+		} else {
+			acc = fn(acc, this[i], i, this);
+		}
+	}
+	return acc;
+};
+
+const totalGeral2 = carrinho.map(getTotal).meuReduce(somar);
+console.log(totalGeral2);
