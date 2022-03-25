bfd2ca0d5c7e7d793d5f9e77372c1572db7d830a
Pinto Júnior <junior_pinto@hotmail.com>
2022-03-25 00:07:40 -0300

    ok

--- funcoes/callback_4.js
+++ funcoes/callback_4.js
@@ -13,15 +13,15 @@ const nomeItensValidos = carrinho.filter(qtdeMaiorQueZero).map(getNome);
 
 console.log(nomeItensValidos);
 
-Array.prototype.meuFilter = function (fn) {
+Array.prototype.meuFilter = function(fn) {
 	const novoArray = [];
-	for (let i = 0; i < this.length; i++){
-		if(fn(this[i], i, this)) {
-			novoArray.push(this[i])
+	for (let i = 0; i < this.length; i++) {
+		if (fn(this[i], i, this)) {
+			novoArray.push(this[i]);
 		}
 	}
-	return novoArray
-}
+	return novoArray;
+};
 
 const nomeItensValidos2 = carrinho.meuFilter(qtdeMaiorQueZero).map(getNome);
 console.log(nomeItensValidos2);
